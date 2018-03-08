369c5610e24c5fc00d29f9df42164d53bcc2e91a
Mitchell Stride <mitchellcstride@gmail.com>
2018-03-08 12:45:12 -0330

    added lab4 text code v1

--- PIC16F684/README.md
+++
@@ -1,2 +0,0 @@
-# Emergency-Light
-ENGI5800 design lab to build a emergency light system using available components on a protoboard.
---
+++ PIC16F684/lab4/Lab4 - Charge Notes.asm
@@ -0,0 +1,12 @@
+// From Lab4 Flow Chart
+
+program LAB4_EMERG_LIGHT
+
+'Initializing Variables
+dim data0 as float    'PWM
+dim vadc0 as float    'PWM
+dim data1 as float
+dim vadc1 as float
+dim data2 as float
+dim vadc2 as float
+dim dc1 as float
